310ee83412c2e867363c6ed22601c80dda9cb042
Airme1 <emmanuelochoja@yahoo.com>
2024-04-09 12:57:51 +0100

    ES lint

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,5 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = ...employeesList;
+  const numberOfDepartments = employeesList.length;
+  return { allEmployees, numberOfDepartments };
+}
