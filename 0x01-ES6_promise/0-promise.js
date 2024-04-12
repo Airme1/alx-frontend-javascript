36ee110873730823a6b617e3d7e5540059e4cf99
Airme1 <emmanuelochoja@yahoo.com>
2024-04-12 19:12:41 +0100

    modify with if_else

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,9 @@
 export default function getResponseFromAPI() {
   return new Promise((myResolve, myReject) => {
-    myResolve("good");
-    myReject("bad");
+    if (true) {
+      myResolve("good");
+    } else {
+      myReject("bad");
+    }
   });
 }
