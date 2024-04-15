62b7ff58ae25fcd815518b177a10610621608c7a
Airme1 <emmanuelochoja@yahoo.com>
2024-04-15 13:37:18 +0100

    changed to resolve method

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((myResolve, myReject) => {
-    myResolve({ firstName: `${firstName}`, lastName: `${lastName}` });
-  });
+  return Promise.resolve({ firstName, lastName });
 }
