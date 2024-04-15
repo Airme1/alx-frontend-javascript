44574a7bec2ea65f58edb00bd9fae45a59ee9ee7
Airme1 <emmanuelochoja@yahoo.com>
2024-04-15 12:45:43 +0100

    from Error to console log

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,11 @@
 import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then(
-    (values) =>
+  Promise.all([uploadPhoto(), createUser()])
+    .then((values) =>
       console.log(
         `${values[0].body} ${values[1].firstName} ${values[1].lastName}`
-      ))
-      .catch(()=> Error("Signup system offline"))
+      )
+    )
+    .catch(() => console.log("Signup system offline"));
 }
