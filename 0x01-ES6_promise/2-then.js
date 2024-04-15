f6a353b79da285a56533137dc2d5bf0a2b8f2f5e
Airme1 <emmanuelochoja@yahoo.com>
2024-04-15 12:07:56 +0100

    returned the promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,12 @@
 export default function handleResponseFromAPI(promise) {
-  let myPromise = new Promise((myResolve, myReject) => {
+  return new Promise((myResolve, myReject) => {
     if (promise) {
       myResolve({ status: 200, body: "success" });
     } else {
       myReject({});
     }
-  });
-
-  myPromise
-    .then((success) => console.log("Got a response from the API"))
-    .catch((error) => console.log("Got a response from the API"));
+  }).then(
+    (success) => console.log("Got a response from the API"),
+    (error) => console.log("Got a response from the API")
+  );
 }
