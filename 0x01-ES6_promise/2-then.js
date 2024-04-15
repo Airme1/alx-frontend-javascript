986e798af4066a5d8a6babd556e02122201c8321
Airme1 <emmanuelochoja@yahoo.com>
2024-04-15 12:30:42 +0100

    then catch finally handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((myResolve, myReject) => {
-    if (promise) {
-      myResolve({ status: 200, body: "success" });
-    } else {
-      myReject({});
-    }
-  }).then(
-    (success) => console.log("Got a response from the API"),
-    (error) => console.log("Got a response from the API")
-  );
+  return promise
+    .then({ status: 200, body: "success" })
+    .catch(new Error())
+    .finally(console.log("Got a response from the API"));
 }
