092f5373cd8378ca5c2e9da0084eebbe3569f56d
Airme1 <emmanuelochoja@yahoo.com>
2024-04-18 03:56:27 +0100

    task 2 extending Teacher with Director

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,13 +8,28 @@ interface Teacher {
 
 }
 
-const teacher3: Teacher = {
+// Director interface to extend Teacher
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+
+  console.log(director1);
+
+
+  const teacher3: Teacher = {
     firstName: 'John',
     fullTimeEmployee: false,
     lastName: 'Doe',
     location: 'London',
     contract: false,
-  };
-  
-  console.log(teacher3);
-  
+};
+
+console.log(teacher3);
