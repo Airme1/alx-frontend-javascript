327368e03d2bbbbfd200188558760e059814ad72
Airme1 <emmanuelochoja@yahoo.com>
2024-04-26 13:47:55 +0100

    update

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((myResolve, myReject) => {
-    if (success) {
-      myResolve({ status: 200, body: "Success" });
+    if (success === true) {
+      myResolve({ status: 200, body: 'Success' });
     } else {
-      myReject(new Error("The fake API is not working currently"));
+      myReject(new Error('The fake API is not working currently'));
     }
   });
 }
