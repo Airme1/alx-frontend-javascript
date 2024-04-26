8ca9ecf1275ed161dea10027e75d20a688d88c9b
Airme1 <emmanuelochoja@yahoo.com>
2024-04-26 14:05:04 +0100

    update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   let container = [];
@@ -8,7 +8,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     uploadPhoto(fileName),
   ]).then((values) => {
     for (const item of values) {
-      if (item.status === "fulfilled") {
+      if (item.status === 'fulfilled') {
         container.push({ status: item.status, value: item.value });
       } else {
         container.push({ status: item.status, value: `${item.reason}` });
